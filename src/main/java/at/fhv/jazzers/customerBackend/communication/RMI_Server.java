8e92daf5d7899407e92e512c7a2203323370a3b4
rhokkar <cpe2877@students.fhv.at>
2022-04-07 00:16:26 +0200

    - fixed custom ports

--- src/main/java/at/fhv/jazzers/customerBackend/communication/RMI_Server.java
+++ src/main/java/at/fhv/jazzers/customerBackend/communication/RMI_Server.java
@@ -9,14 +9,20 @@ import java.rmi.registry.LocateRegistry;
 
 public class RMI_Server {
     public static void start() throws RemoteException, MalformedURLException {
+        // Remote Objects try to listen to docker ip, but they need to listen to localhost (local .env) or application-server-ip (azure .env)
+        System.setProperty("java.rmi.server.hostname", System.getenv("CUSTOMER_RMI_HOST"));
+        System.setProperty("com.sun.management.jmxremote.port", System.getenv("CUSTOMER_RMI_PORT"));
+        System.setProperty("com.sun.management.jmxremote.rmi.port", System.getenv("CUSTOMER_RMI_PORT"));
+        // System.setProperty("com.sun.management.jmxremote", "true");
+        // System.setProperty("com.sun.management.jmxremote.local.only", "false");
+        System.setProperty("com.sun.management.jmxremote.authenticate", "false");
+        System.setProperty("com.sun.management.jmxremote.ssl", "false");
+
         // Create Registry
         LocateRegistry.createRegistry(Integer.parseInt(System.getenv("CUSTOMER_RMI_PORT")));
 
-        // Remote Objects try to listen to docker ip, but they need to listen to localhost (local .env) or application-server-ip (azure .env)
-        System.setProperty("java.rmi.server.hostname", System.getenv("CUSTOMER_RMI_HOST"));
-
         // Bind Services
-        Naming.rebind("rmi://localhost/customerService", ServiceRegistry.rmi_customerService());
+        Naming.rebind("rmi://localhost:1100/customerService", ServiceRegistry.rmi_customerService());
         System.out.println("Customer Service bound on PORT " + System.getenv("CUSTOMER_RMI_PORT"));
     }
 }
